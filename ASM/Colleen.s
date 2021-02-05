f547fa78cc1692c39bc59f07c771e0b6f6ccf1fb
pankratdodo <PW8A27D-6c&a>
2021-02-05 20:26:18 +0300

    refactor colleen asm

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -2,7 +2,7 @@
 global _main
 extern _printf
 section .data
-str: db "; Comment%2$cglobal _main%2$cextern _printf%2$csection .data%2$ccode: db %3$c%1$s%3$c%2$csection .text%2$cprint:%2$cpush rbx%2$cmov rdi, code%2$cmov rsi, code%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$cpop rbx%2$cret%2$c_main:%2$c;Comment inside%2$cpush rbx%2$ccall print%2$cpop rbx%2$cret%2$c"
+str: db "; Comment%2$cglobal _main%2$cextern _printf%2$csection .data%2$cstr: db %3$c%1$s%3$c%2$csection .text%2$cprint:%2$cpush rbx%2$cmov rdi, str%2$cmov rsi, str%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$cpop rbx%2$cret%2$c_main:%2$c;Comment inside%2$cpush rbx%2$ccall print%2$cpop rbx%2$cret%2$c"
 section .text
 print:
 push rbx
